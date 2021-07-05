df7a8729f3a4053df4aa9a089fc215ae832056fc
naghmakhan16764 <74590340+naghmakhan16764@users.noreply.github.com>
2021-07-05 21:49:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,93 +1,52 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Constraint = Matter.Constraint;
-var boy,ground,rock,tree,m1,m2,m3,m4,m5,m6,m7,chain;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var game
 
-function preload()
-{
-	boy = loadImage("boy.png");
-	tree = loadImage("tree.png");
-	
+
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
 }
-
 function setup() {
-	createCanvas(1280, 400);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-	rock = new stone(320,225);
-	chain = new SlingShot(rock.body,{x:320 , y:225});
-	m1 = new mango(900,150,8);
-	m2 = new mango(950,120,10);
-	m3 = new mango(1000,100,7);
-	m4 = new mango(950,60,9);
-	m5 = new mango(1050,60,6);
-	m6 = new mango(1100,120,10);
-	ground = Bodies.rectangle(640,385,1290,20,{isStatic:true});
-	World.add(world,ground);
-	
-	Engine.run(engine);
-	  
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
 }
-function mouseDragged(){
-    Matter.Body.setPosition(rock.body,{x:mouseX,y:mouseY});
-}
-function mouseReleased(){
-    chain.fly();
-}
-
 
 function draw() {
-	if (keyIsPressed === true) {
-		chain.attach();
-	  }
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
 
-    rectMode(CENTER);
-    push();
-    rect(width/2,400,width,20);
-	chain.display();
-
-	
-    drawSprites();
-
-
-
-
-    console.log(rock);
-    imageMode(CENTER);
-
-
-    image(boy,400,300,250,300);
-	image(tree,1000,180,400,400);
-
-
-
-	rock.display();
-	m1.display();
-	m2.display();
-	m3.display();
-	m4.display();
-	m5.display();
-	m6.display();
-	collision(rock,m1);
-	collision(rock,m2);
-	collision(rock,m3);
-	collision(rock,m4);
-	collision(rock,m5);
-	collision(rock,m6);
-
-	
-	
-}
-function collision(a,b){
-	var d = dist(a.body.position.x,a.body.position.y,b.body.position.x,b.body.position.y)
-	if(d <= 50){
-		Body.setStatic(b.body,false);
-	}
+   
 }
